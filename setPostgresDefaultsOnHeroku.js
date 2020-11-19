1b5433eccb83861e33cf26964d19aa8fd29217c6
Karl Horky <karl.horky@gmail.com>
2020-11-19 13:37:29 +0100

    Add link and comment

--- setPostgresDefaultsOnHeroku.js
+++ setPostgresDefaultsOnHeroku.js
@@ -2,12 +2,12 @@ module.exports = function setPostgresDefaultsOnHeroku() {
   if (process.env.DATABASE_URL) {
     const url = require('url');
 
-    // Extract the connection information from the Heroku
-    // environment variable
+    // Extract the connection information from the Heroku environment variable
+    // https://devcenter.heroku.com/changelog-items/438
     const { hostname, pathname, auth } = url.parse(process.env.DATABASE_URL);
-
     const [username, password] = auth.split(':');
 
+    // Set standard environment variables
     process.env.PGHOST = hostname;
     process.env.PGDATABASE = pathname.slice(1);
     process.env.PGUSERNAME = username;
